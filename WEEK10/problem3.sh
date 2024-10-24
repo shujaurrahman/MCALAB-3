70a38c26c55e948790d60c59afb03bd4d6b1aa11
shujaurrahman <Shujaurrehman210@gmail.com>
2024-10-24 19:44:29 +0530

    week 10

---
+++ WEEK10/problem3.sh
@@ -0,0 +1,11 @@
+name=”Shuja ur ragman”
+age=25
+city=”Pilibhit”
+
+echo “Name : $name”
+echo “Age : $age”
+echo “City : $city”
+
+age_1=$((age+1))
+
+echo “Next year, $name will be $age_1 years old.”
